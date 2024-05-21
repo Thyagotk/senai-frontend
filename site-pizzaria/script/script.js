751de309cf09e7ba2127edb4b0db8da76644f8db
Thyago Nascimento <tekinthyago@gmail.com>
2024-05-20 21:47:47 -0300

    Carregar produtos apartir de uma lista jason

--- site-pizzaria/script/script.js
+++ site-pizzaria/script/script.js
@@ -6,4 +6,28 @@ menuToggle.addEventListener('click', () => {
     document.body.style.overflow = show ? 'hidden' : 'initial';
     menuContent.classList.toggle('on', show);
     show = !show;
-})
+});
+
+let intensCardapio = document.getSelection(".itens-cardapio");
+for(const p of produtos) {
+    intensCardapio.InnerHTML +=
+    div class=""
+    <img src="imagens/pngtree-cartoon-yellow-pizza-illustration-png-image_4634672-removebg-preview.png"
+            alt="">
+        <h3>PizzaShop</h3>
+    </div>
+    <ul class="list-menu">
+        <li><a href="#home">Home</a></li>
+        <li><a href="#sobre-nos">Sobre Nós</a></li>
+        <li><a href="#cardapio">Cardápio</a></li>
+        <li><a href="#contatos">Contatos</a></li>
+        <li><a href=""><i class="bi bi-instagram"></i></a></li>
+        <li><a href=""><i class="bi bi-facebook"></i></a></li>
+        <li><a href=""><i class="bi bi-cart2"></i></a></li>
+    </ul>
+    <a href="#home">
+        <div class="menu-toggle">
+            <div class="one"></div>
+
+
+}
