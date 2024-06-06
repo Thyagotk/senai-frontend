1cb0bd157ad0b7709a5a2a26b998b83fd51dc61f
Thyago Nascimento <tekinthyago@gmail.com>
2024-06-05 21:11:25 -0300

    atualização

---
+++ site-de-venda/produtos.js
@@ -0,0 +1,41 @@
+let produtos = [
+    {
+        img:"img/01.PNG",
+        dsImg: "Vela 01",
+        nome: "Banho de Mar 300g",
+        valor:"R$119,00",
+    },
+
+    {
+        img:"img/02.PNG",
+        dsImg: "Vela 02",
+        nome: "Passeio na Floresta 300g",
+        valor:"R$99,00",
+    },
+    {
+        img:"img/03.PNG",
+        dsImg: "Vela 03",
+        nome: "Trio Bom dia 300g",
+        valor:"R$89,00",
+    },
+
+    {
+        img:"img/04.PNG",
+        dsImg: "Vela 04",
+        nome: "Meus Cristais 300g",
+        valor:"R$135,00",
+    },
+
+    {
+        img:"img/05.PNG",
+        dsImg: "Vela 05",
+        nome: "Campo de Lavanda 300g",
+        valor:"R$109,00",
+    },
+
+    {
+        img:"img/06.PNG",
+        dsImg: "Vela 06",
+        nome: "Minha Rotina 300g",
+        valor:"R$149,00",
+    }]
