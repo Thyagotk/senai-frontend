da00f6718153f9b3622fc717a30eaf159dab9547
Thyago Nascimento <tekinthyago@gmail.com>
2024-06-13 19:42:39 -0300

    Finalizando projeto

---
+++ projeto/e-commerce/scripts/produtos.js
@@ -0,0 +1,57 @@
+let produtos = [
+    
+    {
+        img: "imagens/vela02.jpg",
+        dsImg: "Vela Amêndoa",
+        nome: "Vela Amêndoa Vegana- 100G",
+        valor: 55.90   
+    },
+    {
+        img: "imagens/vela03.jpg",
+        dsImg: "Kit Vela",
+        nome: "Kit com 03 velas vegetais - 300G",
+        valor: 129.99   
+    },
+    {
+        img: "imagens/vela04.jpg",
+        dsImg: "Vela Sândalo",
+        nome: "Vela Sândalo - 200G",
+        valor: 125.48
+    },
+    {
+        img: "imagens/vela05.jpg",
+        dsImg: "Vela Cidreira",
+        nome: "Vela Cidreira - 150G",
+        valor: 85.00   
+    },
+    {
+        img: "imagens/vela06.jpg",
+        dsImg: "Vela Citronela",
+        nome: "Vela Citronela - 200G",
+        valor: 49.99    
+    },
+    {
+        img: "imagens/vela01.jpg",
+        dsImg: "Vela Alecrim",
+        nome: "Vela Alecrim - 150G",
+        valor: 64.99    
+    },
+    {
+        img: "imagens/vela07.jpg",
+        dsImg: "Vela Bothânica Vegana",
+        nome: "Vela Bothânica Vegana - 150G",
+        valor: 79.99
+    },
+    {
+        img: "imagens/vela08.jpg",
+        dsImg: "Vela Limão Siciliano",
+        nome: "Vela Limão Siciliano - 150G",
+        valor: 85.99
+    },
+    {
+        img: "imagens/vela09.jpg",
+        dsImg: "Vela Baunilha Vanilla",
+        nome: "Vela Baunilha Vanilla - 250G",
+        valor: 55.99
+    },
+    ]
